c628bf9e32fede391d2b00246b9eb712899430da
melvyn2 <melvyn.depeyrot@gmail.com>
2018-07-19 16:30:05 -0700

    More error handling, backup codes, base rename

--- PySteamAuth/PyUIs/SetupDialog.py
+++ PySteamAuth/PyUIs/SetupDialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/Users/melvyn/PythonSteamAuth-GUI/PySteamAuth/UIs/SetupDialog.ui'
+# Form implementation generated from reading ui file '/Users/melvyn/PySteamAuth/PySteamAuth/UIs/SetupDialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
